1d060df253a69b953f961144c040b293f0f603e6
Simon Eisenmann <simon@longsleep.org>
2018-04-25 18:21:50 +0200

    Filter constraints to available devices
    When a device does not have cam/mic it does not make sense to request
    the constraint for it. This also is often harmful as the constraint
    cannot be met and the whole gUM call fails. This change checks if the
    device has at least one audio or video input and filters the requested
    gUM constraints if no matching device is available.

--- src/actions/usermedia.js
+++ src/actions/usermedia.js
@@ -16,7 +16,7 @@ const getSupportedConstraints = () => {
 };
 export const supportedConstraints = getSupportedConstraints();
 
-/*const enumerateDevices = async () => {
+const enumerateDevices = async () => {
   const devices = await navigator.mediaDevices.enumerateDevices();
 
   let videoSource = null;
@@ -33,7 +33,7 @@ export const supportedConstraints = getSupportedConstraints();
     videoSource,
     audioSource,
   };
-};*/
+};
 
 export function requestUserMedia(id='', video=true, audio=true) {
   let status = requestUserMediaStatus[id];
@@ -45,67 +45,88 @@ export function requestUserMedia(id='', video=true, audio=true) {
   }
 
   const idx = ++status.idx;
-  console.info('requestUserMedia', idx, status, video, audio); // eslint-disable-line no-console
+  console.info('requestUserMedia request', idx, status, {video, audio}); // eslint-disable-line no-console
 
   return async dispatch => {
-    // Generate constraints syntax according to the standard.
-    // https://w3c.github.io/mediacapture-main/#constrainable-interface
-    const constraints = {};
-    const videoConstraints = {
-      advanced: [],
-    };
-    const audioConstraints = {
-      advanced: [],
-    };
-    if (video) {
-      if (supportedConstraints.width && supportedConstraints.height) {
-        videoConstraints.width = {
-          ideal: 640,
-        };
-        videoConstraints.height = {
-          ideal: 360,
-        };
+    // Generate constraints for requested devices filtered by existing devices.
+    return enumerateDevices().then(({ videoSource, audioSource }) => {
+      console.info('requestUserMedia devices', idx, status, // eslint-disable-line no-console
+        {video: videoSource && true, audio: audioSource && true});
+      if (idx !== status.idx) {
+        console.warn('requestUserMedia is outdated after enumerateDevices', // eslint-disable-line no-console
+          idx, status);
+        return null;
       }
-      if (supportedConstraints.frameRate) {
-        videoConstraints.frameRate = {
-          min: 10,
-          ideal: 15,
-        };
-      }
-      if (supportedConstraints.facingMode) {
-        videoConstraints.advanced.push({facingMode: 'user'});
-      }
-    }
 
-    // Apply.
-    if (video) {
-      constraints.video = videoConstraints;
-    } else
-      constraints.video = false;
-    if (audio) {
-      constraints.audio = audioConstraints;
-    } else {
-      constraints.audio = false;
-    }
-
-    console.log('gUM constraints', constraints); // eslint-disable-line no-console
-
-    return navigator.mediaDevices.getUserMedia(constraints)
-      .then((stream) => {
-        console.log('gUM stream', idx, stream); // eslint-disable-line no-console
-        if (idx !== status.idx) {
-          stopUserMediaStream(stream);
-          return null;
+      // Generate constraints syntax according to the standard.
+      // https://w3c.github.io/mediacapture-main/#constrainable-interface
+      const constraints = {};
+      const videoConstraints = {
+        advanced: [],
+      };
+      const audioConstraints = {
+        advanced: [],
+      };
+      if (video) {
+        if (supportedConstraints.width && supportedConstraints.height) {
+          videoConstraints.width = {
+            ideal: 640,
+          };
+          videoConstraints.height = {
+            ideal: 360,
+          };
         }
-
-        if (status.stream) {
-          // Stop previous stream.
-          stopUserMediaStream(status.stream);
+        if (supportedConstraints.frameRate) {
+          videoConstraints.frameRate = {
+            min: 10,
+            ideal: 15,
+          };
         }
-        status.stream = stream;
-        dispatch(userMediaAudioVideoStream(id, stream));
-        return stream;
-      });
+        if (supportedConstraints.facingMode) {
+          videoConstraints.advanced.push({facingMode: 'user'});
+        }
+      }
+
+      // Apply.
+      if (video && videoSource) {
+        constraints.video = videoConstraints;
+      } else
+        constraints.video = false;
+      if (audio && audioSource) {
+        constraints.audio = audioConstraints;
+      } else {
+        constraints.audio = false;
+      }
+
+      console.log('gUM constraints', constraints); // eslint-disable-line no-console
+      return constraints;
+    }).then(constraints => {
+      // Request user media for the provided constraints.
+      if (constraints === null) {
+        // No constraints, do not request gUM, directly pretend to have no stream.
+        console.warn('requestUserMedia no constraints', // eslint-disable-line no-console
+          idx, status);
+        return null;
+      }
+      return navigator.mediaDevices.getUserMedia(constraints);
+    }).then(stream => {
+      // Process stream.
+      console.log('gUM stream', idx, stream); // eslint-disable-line no-console
+      if (idx !== status.idx) {
+        console.warn('requestUserMedia is outdated after gUM', // eslint-disable-line no-console
+          idx, status);
+        stopUserMediaStream(stream);
+        return null;
+      }
+
+      if (status.stream) {
+        // Stop previous stream.
+        stopUserMediaStream(status.stream);
+      }
+      status.stream = stream;
+      dispatch(userMediaAudioVideoStream(id, stream));
+      return stream;
+    });
   };
 }
 
