ced74741487af38ba020bcab5c4b5a976da41345
Simon Eisenmann <simon@longsleep.org>
2018-05-04 13:18:54 +0200

    Add unit test which uses kpop
    This unit test currently fails as Jest cannot deal with ES6 and
    react-scripts does not transpile external dependencies. This is kind of
    a problem right now we cannot solve without either forking
    create-react-app or publish an ES5 version of kpop.

--- src/components/CallView.test.js
+++ src/components/CallView.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import HowlingProvider from './provider';
+import CallView from './CallView';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <HowlingProvider src="mock.ogg">
-      <span/>
-    </HowlingProvider>, div);
+    <CallView></CallView>, div);
 });
