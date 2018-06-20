db1dc3dbcdcc7c5cfd8031f41bfb6735964ec995
Simon Eisenmann <simon@longsleep.org>
2018-06-20 16:24:46 +0200

    Always add a dummy stream for peers

--- src/reducers/streams.js
+++ src/reducers/streams.js
@@ -1,14 +1,30 @@
 import {
+  KWM_CALL_NEW,
   KWM_CALL_DESTROY,
   KWM_STREAM_RECEIVED,
 } from '../actions/types';
 
+const defaultMediaStream = new MediaStream();
 
 const defaultState = {
 };
 
 function streamsReducer(state = defaultState, action) {
   switch (action.type) {
+    case KWM_CALL_NEW: {
+      if (state[action.record.user]) {
+        // We already know this user, do nothing.
+        return state;
+      }
+      const entry = {
+        id: action.record.user,
+        stream: defaultMediaStream,
+      };
+      return Object.assign({}, state, {
+        [action.record.user]: entry,
+      });
+    }
+
     case KWM_CALL_DESTROY: {
       const streams = Object.assign({}, state);
       delete streams[action.record.user];
