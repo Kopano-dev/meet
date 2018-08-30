8f70d12bda6239b7125f6b115388bc4fd19918e5
Simon Eisenmann <simon@longsleep.org>
2018-08-30 15:37:36 +0200

    Fetch up to 1000 contacts from users endpoint
    To avoid loading a large number of contacts, this change limits the
    result set to 1000. Eventually this needs to evolve into a server side
    search.

--- src/api.js
+++ src/api.js
@@ -5,8 +5,11 @@ export function fetchUsers() {
   return (dispatch, getState) => {
     const { config, user } = getState().common;
 
+    // TODO(longsleep): Add support for use cases when there are more than
+    // 1000 users. Needs a logic change and rely exclusively on searching in
+    // such cases.
     return dispatch(networkFetch(
-      config.apiPrefix + '/users', {
+      config.apiPrefix + '/users?$top=1000', {
         method: 'GET',
         headers: getHeadersFromConfig(config, user),
       }
