d9a715dbedba9f125b89dddedf03bd3f3bed4429
Simon Eisenmann <simon@longsleep.org>
2018-09-04 16:17:32 +0200

    Use grapi ID to connect to KWM

--- src/components/KWMProvider.js
+++ src/components/KWMProvider.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { setupKWM } from '../actions/kwm';
+import { getOwnGrapiUserEntryID } from '../selectors';
 
 class KWMProvider extends React.PureComponent {
   state = {
@@ -12,11 +13,13 @@ class KWMProvider extends React.PureComponent {
   }
 
   static getDerivedStateFromProps(props, state) {
-    let { id,  authorizationType, authorizationValue } = state;
-    let { user } = props;
+    let { id, authorizationType, authorizationValue } = state;
+    let { user, grapiID } = props;
 
     if (user && user.profile && user.access_token) {
-      id = user.profile.sub;
+      // NOTE(longsleep): Get Kopano Groupware API user first, otherwise use
+      // the sub from OIDC>
+      id = grapiID ? grapiID : user.profile.sub;
       authorizationType = user.token_type;
       authorizationValue = user.access_token;
     } else {
@@ -73,13 +76,16 @@ class KWMProvider extends React.PureComponent {
 
 KWMProvider.propTypes = {
   user: PropTypes.object,
+  grapiID: PropTypes.string,
 };
 
 const mapStateToProps = state => {
   const { user } = state.common;
+  const grapiID = getOwnGrapiUserEntryID(state);
 
   return {
     user,
+    grapiID,
   };
 };
 
