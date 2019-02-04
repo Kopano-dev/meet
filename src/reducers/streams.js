3719057779f0dc3d6e8f1c0b893b503c5c19cb53
Simon Eisenmann <simon@longsleep.org>
2019-02-04 18:42:23 +0100

    Use kwm profile displayName as stream displayName
    KWM might be able to provide a displayName for streams together with
    incoming and outgoing call events. This change uses that value if a
    stream has no displayName from another source already. This is
    especially helpful when the client runs as guest without any contacts or
    when a peer is no contact.

--- src/reducers/streams.js
+++ src/reducers/streams.js
@@ -3,6 +3,8 @@ import { forceBase64StdEncoded } from 'kpop/es/utils';
 import {
   KWM_CALL_NEW,
   KWM_CALL_DESTROY,
+  KWM_CALL_INCOMING,
+  KWM_CALL_OUTGOING,
   KWM_STREAM_RECEIVED,
   CONTACTS_UPDATE,
 } from '../actions/types';
@@ -27,6 +29,28 @@ function streamsReducer(state = defaultState, action) {
       };
       return Object.assign({}, state, {
         [action.record.user]: entry,
+      });
+    }
+
+    case KWM_CALL_INCOMING:
+    case KWM_CALL_OUTGOING: {
+      // Update display names from record, if any and current entry is empty.
+      if (!action.record.profile) {
+        return state;
+      }
+      const entry = state[action.record.user];
+      if (!entry) {
+        // We no nothing about this user, do nothing.
+        return state;
+      }
+      if (entry.user && entry.user.displayName !== '') {
+        // Already got a name.
+        return state;
+      }
+      return Object.assign({}, state, {
+        [action.record.user]: Object.assign({}, entry, {
+          user: {displayName: action.record.profile ? action.record.profile.name : ''},
+        }),
       });
     }
 
