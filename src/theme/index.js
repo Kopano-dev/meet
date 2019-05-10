ff916287eb05fc49eeb4f4936c48c6780e5d6b66
Simon Eisenmann <simon@longsleep.org>
2019-05-10 16:58:42 +0200

    Use Meet colors in theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,32 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import kopanoGreen from 'kpop/es/colors/kopanoGreen';
+
 const darkGrey = '#272c2e';
 const lightBlue = '#3eb4f3';
 const lightGrey = '#c1c1c1';
 
-const meetTheme = outerThemme => ({
-  ...outerThemme,
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: kopanoGreen[400],
+      main: kopanoGreen[500],
+      dark: kopanoGreen[600],
+    },
+    // NOTE(longsleep): KopanoGreen is too light and thus needs 2.0 contrastThreshold
+    // to make sure the default 500 color is still using white text. It will
+    // show warnings in development mode that the contrast is too low as W3C
+    // recommends the threshold to be 3 or more. This cannot be helped.
+    contrastThreshold: 2.0,
+  },
+});
+
+const meetTheme = outerTheme => ({
+  ...outerTheme,
+  palette: {
+    ...outerTheme.palette,
+    primary: {...theme.palette.primary},
+  },
   callBackground: {
     top: lightBlue,
     bottom: darkGrey,
