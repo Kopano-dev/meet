f4c8cb40f403202aefc555f3717f5bcb1a01459b
Simon Eisenmann <simon@longsleep.org>
2019-05-10 16:44:03 +0200

    Only enable i18n for languages which are complete

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,8 +1,6 @@
 // NOTE(longsleep): This loads all translation files to be included in the
 // app bundle. They are not that large.
 
-import { defineLocales } from 'kpop/i18n/locales';
-
 // Please keep imports and exports alphabetically sorted.
 import de from './de.json';
 import es from './es.json';
@@ -15,7 +13,18 @@ import nl from './nl.json';
 import ptPT from './pt_PT.json';
 import ru from './ru.json';
 
-const locales = {
+function enableLocales(locales, enabled) {
+  if (process.env.NODE_ENV !== 'production') { // eslint-disable-line no-undef
+    return locales;
+  }
+  return enabled.reduce(function(value, locale) {
+    value[locale] = locales[locale];
+    return value;
+  }, {});
+}
+
+// Locales must follow BCP 47 format (https://tools.ietf.org/html/rfc5646).
+const locales = enableLocales({
   de,
   es,
   fr,
@@ -24,9 +33,13 @@ const locales = {
   ja,
   nb,
   nl,
-  pt: ptPT,
-  'pt-pt': ptPT,
+  'pt-PT': ptPT,
   ru,
-};
+}, [
+  // List of enabled languages in production builds.
+  'de',
+  'en-GB',
+  'en-US',
+]);
 
-export default defineLocales(locales);
+export default locales;
