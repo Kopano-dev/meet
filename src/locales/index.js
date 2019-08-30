c14748fc5ee294ec78a91b391395f34ef15d7ced
Simon Eisenmann <simon@longsleep.org>
2019-08-30 17:38:00 +0200

    Enable more translations

--- src/locales/index.js
+++ src/locales/index.js
@@ -5,13 +5,18 @@
 import de from './de.json';
 import es from './es.json';
 import fr from './fr.json';
+import hi from './hi.json';
 import hu from './hu.json';
+import hr from './hr.json';
+import is from './is.json';
 import it from './it.json';
 import ja from './ja.json';
 import nb from './nb.json';
 import nl from './nl.json';
+import pl from './pl.json';
 import ptPT from './pt_PT.json';
 import ru from './ru.json';
+import sl from './sl.json';
 
 function enableLocales(locales, enabled) {
   if (process.env.NODE_ENV !== 'production') { // eslint-disable-line no-undef
@@ -28,19 +33,28 @@ const locales = enableLocales({
   de,
   es,
   fr,
+  hi,
   hu,
+  hr,
+  is,
   it,
   ja,
   nb,
   nl,
+  pl,
   'pt-PT': ptPT,
   ru,
+  sl,
 }, [
   // List of enabled languages in production builds.
   'de',
   'en-GB',
   'en-US',
+  'fr',
+  'nb',
   'nl',
+  'pt-PT',
+  'ru',
 ]);
 
 export default locales;
--- src/moment.js
+++ src/moment.js
@@ -1,8 +1,12 @@
 import moment from 'moment';
-import 'moment/locale/en-gb';
+import 'moment/locale/en-gb'; // Keep en-gb first.
+
+// Additional languages - please keep alphabetically sorted and only import codes which are in production builds.
 import 'moment/locale/de';
-import 'moment/locale/nl';
 import 'moment/locale/fr';
-import 'moment/locale/es';
+import 'moment/locale/nb';
+import 'moment/locale/nl';
+import 'moment/locale/pt'; // This is pt-pt.
+import 'moment/locale/ru';
 
 export default moment;
