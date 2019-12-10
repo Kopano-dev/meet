4a2bf2838f60607a73eae28d3efa62d19870bb26
Simon Eisenmann <s.eisenmann@kopano.com>
2019-12-10 19:54:29 +0100

    Ensure to activate new service worker on update
    If an old service worker is active it also means that an old version of
    Meet is active which does potentially not support the skip waiting
    trigger itself. Thus for now we always activate our new service worker
    directly.

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,1 +1,6 @@
+// NOTE(longsleep): This is needed, since we cannot rely on an existing installed
+// service worker to send the SKIP_WAITING message. Unfortunate, but for now
+// this fixes update until a better update path can be found.
+self.skipWaiting();
+
 /* Additional service worker content goes here. */
