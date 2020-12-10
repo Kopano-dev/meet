6bfb144351440ed47ec845308d7d02b44baeadea
Simon Eisenmann <s.eisenmann@kopano.com>
2020-12-10 18:48:40 +0100

    Ensure the call timer always updates as needed
    It was seen that the in call timer does sometimes not get updates every
    second. This change moves the implementation to a more efficient
    solution which should solve the issue.

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,32 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import moment from '../moment';
 
-const Timer = (props) => {
-  const { start } = props;
-
-  const [now, setNow] = React.useState(moment());
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setNow(moment());
-    }, 1000);
-    return function cleanup() {
-      clearInterval(timer);
-    };
-  });
-
-  const duration = moment.duration(now.diff(start));
-  const hours = parseInt(duration.as('hours'), 10);
-
-  const result = [];
-  if (hours > 0) {
-    result.push(String(hours));
+class Timer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.timestamp = undefined;
+    this.stopped = false;
   }
-  return [
-    ...result,
-    ('0' + duration.minutes()).slice(-2),
-    ('0' + duration.seconds()).slice(-2),
-  ].join(':');
+
+  updateTime = (timestamp) => {
+    if (this.stopped) {
+      return;
+    }
+
+    if (this.timestamp === undefined) {
+      this.timestamp = timestamp;
+    }
+
+    const elapsed = timestamp - this.timestamp;
+    if (elapsed >= 500) {
+      this.ref.current.innerHTML = this.formatTimeString();
+    }
+
+    window.requestAnimationFrame(this.updateTime);
+  }
+
+  formatTimeString = () => {
+    const { start } = this.props;
+
+    const now = moment();
+    const duration = moment.duration(now.diff(start));
+    const hours = parseInt(duration.as('hours'), 10);
+
+    const result = [];
+    if (hours > 0) {
+      result.push(String(hours));
+    }
+    return [
+      ...result,
+      ('0' + duration.minutes()).slice(-2),
+      ('0' + duration.seconds()).slice(-2),
+    ].join(':');
+  }
+
+  componentDidMount() {
+    this.updateTime();
+  }
+
+  render() {
+    return <span ref={this.ref}>{this.formatTimeString()}</span>;
+  }
+
+  componentWillUnmount() {
+    this.stopped = true;
+  }
+}
+
+Timer.propTypes = {
+  start: PropTypes.object.isRequired,
 };
 
 export default Timer;
